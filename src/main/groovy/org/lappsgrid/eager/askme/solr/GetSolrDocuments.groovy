c878cf781b4e347043b50343f1180fc86ad1f6f7
Kevin Ros <kevinros1190@gmail.com>
2019-07-23 20:43:57 -0400

    style updates

--- src/main/groovy/org/lappsgrid/eager/askme/solr/GetSolrDocuments.groovy
+++ src/main/groovy/org/lappsgrid/eager/askme/solr/GetSolrDocuments.groovy
@@ -2,56 +2,52 @@ package org.lappsgrid.eager.askme.solr
 
 import groovy.util.logging.Slf4j
 import org.apache.solr.client.solrj.SolrClient
-import org.apache.solr.client.solrj.SolrRequest
-import org.apache.solr.client.solrj.SolrResponse
-import org.apache.solr.client.solrj.SolrServerException
 import org.apache.solr.client.solrj.impl.CloudSolrClient
 import org.apache.solr.client.solrj.response.QueryResponse
-import org.apache.solr.common.SolrDocument
 import org.apache.solr.common.SolrDocumentList
 import org.apache.solr.common.params.MapSolrParams
-import org.apache.solr.common.params.SolrParams
-import org.apache.solr.common.util.NamedList
 import org.lappsgrid.eager.mining.api.Query
-
-
 
 @Slf4j("logger")
 class GetSolrDocuments {
 
-    //DOES THIS NEED TO BE PRIVATE
-    Map answer(Query query) {
+    /**
+     * rows -----------> number of solr documents returned
+     * solr_address ---> web address to solr database
+     * collection -----> the specific database accessed
+     * fl -------------> fields saved from each document in the solr query
+     */
 
-        //init()
-        logger.info("Generating answer.")
+    final Integer rows = 1
+    final String solr_address = "http://129.114.16.34:8983/solr"
+    final String collection = 'bioqa'
+    final String fl = 'pmid,pmc,doi,year,title,path,abstract,body'
 
+
+    Map answer(Query query, String id) {
+
+        logger.info("Generating answer for Message {}", id)
         logger.info("Creating CloudSolrClient")
+        SolrClient solr = new CloudSolrClient.Builder([solr_address]).build()
 
-        SolrClient solr = new CloudSolrClient.Builder(["http://129.114.16.34:8983/solr"]).build()
-
-
-
-        logger.trace("Generating query")
+        logger.info("Generating solr parameters")
         Map solrParams = [:]
         solrParams.q = query.query
-        solrParams.fl = 'pmid,pmc,doi,year,title,path,abstract,body'
-        solrParams.rows = 1
-
+        solrParams.fl = fl
+        solrParams.rows = rows
         MapSolrParams queryParams = new MapSolrParams(solrParams)
-        String collection = 'bioqa'
 
-        logger.trace("Sending query to Solr")
+        logger.info("Sending query to Solr")
         final QueryResponse response = solr.query(collection, queryParams)
         final SolrDocumentList documents = response.getResults()
         int n = documents.size()
-        logger.trace("Received {} documents", n)
+
+        logger.info("Received {} documents", n)
         Map result = [:]
         result.query = query
         result.size = n
         result.documents = documents
-        logger.info('Result: {}', result.documents)
 
         return result
     }
-
 }
--- src/main/groovy/org/lappsgrid/eager/askme/solr/Main.groovy
+++ src/main/groovy/org/lappsgrid/eager/askme/solr/Main.groovy
@@ -1,53 +1,46 @@
 package org.lappsgrid.eager.askme.solr
 
-import org.apache.solr.common.SolrDocumentList
 import org.lappsgrid.eager.mining.api.Query
 import org.lappsgrid.rabbitmq.Message
 import org.lappsgrid.rabbitmq.topic.MessageBox
 import org.lappsgrid.rabbitmq.topic.PostOffice
 import groovy.util.logging.Slf4j
 import org.lappsgrid.eager.mining.core.json.Serializer
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
 
 
-/**
- *
- */
 @Slf4j("logger")
 class Main extends MessageBox{
-    static final String BOX = 'solr.mailbox'
+    static final String MBOX = 'solr.mailbox'
     static final String WEB_MBOX = 'web.mailbox'
     static final String HOST = "rabbitmq.lappsgrid.org"
     static final String EXCHANGE = "org.lappsgrid.query"
-    PostOffice po = new PostOffice(EXCHANGE, HOST)
-
+    static final PostOffice po = new PostOffice(EXCHANGE, HOST)
 
     Main(){
-        super(EXCHANGE, BOX)
+        super(EXCHANGE, MBOX)
     }
 
-
     void recv(Message message){
+        logger.info("Received message {}", message.getId())
+
+        logger.info("Generating query from Message {}", message.getId())
         Query query = Serializer.parse(Serializer.toJson(message.body), Query)
-        logger.debug("Received message, query is: {}",query)
-        logger.debug("{}", Serializer.toJson(message.body))
-        GetSolrDocuments process = new GetSolrDocuments()
+
         logger.info("Gathering solr documents")
-        Map result = process.answer(query)
-        logger.info("Result.query is {}", result.query)
+        GetSolrDocuments process = new GetSolrDocuments()
+        Map result = process.answer(query, message.getId())
         message.setBody(result)
-        logger.info("Processed query, sending documents back to web")
+
+        logger.info("Processed query from Message {}, sending documents back to web", message.getId())
         message.setRoute([WEB_MBOX])
         message.setCommand('solr')
         po.send(message)
+        logger.info("Message {} with solr documents sent back to web", message.getId())
 
     }
-
-
-
     
     static void main(String[] args) {
+        logger.info("Starting solr access module, awaiting message from web module")
         new Main()
     }
 }
