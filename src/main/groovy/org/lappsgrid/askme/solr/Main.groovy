38e27fefe91d00cd96f6e355bf605b54b63cb423
Kevin Ros <kevinros1190@gmail.com>
2019-08-13 10:25:09 -0400

    style changes

--- src/main/groovy/org/lappsgrid/askme/solr/Main.groovy
+++ src/main/groovy/org/lappsgrid/askme/solr/Main.groovy
@@ -2,15 +2,16 @@ package org.lappsgrid.askme.solr
 
 import org.lappsgrid.eager.mining.api.Query
 import org.lappsgrid.rabbitmq.topic.MailBox
-
-//import org.lappsgrid.eager.mining.core.json.Serializer
 import org.lappsgrid.serialization.Serializer
-
 import org.lappsgrid.rabbitmq.Message
-import org.lappsgrid.rabbitmq.topic.MessageBox
 import org.lappsgrid.rabbitmq.topic.PostOffice
 import groovy.util.logging.Slf4j
 
+/**
+ * TODO:
+ * 1) Update imports to phase out eager (waiting on askme-core pom)
+ * 2) Add exceptions / case statements to recv method?
+ */
 
 @Slf4j("logger")
 class Main{
@@ -22,7 +23,6 @@ class Main{
     MailBox box
 
     Main() {
-        //super(EXCHANGE, MBOX)
     }
 
     void run(lock) {
@@ -33,34 +33,28 @@ class Main{
                 String command = message.getCommand()
                 String id = message.getId()
                 Object params = message.getParameters()
-
-                //logger.info("Received message {}", id)
-
                 if (command == 'EXIT' || command == 'STOP') {
                     logger.info('Received shutdown message, terminating Solr service')
                     synchronized(lock) { lock.notify() }
                 }
                 else if(command == 'PING') {
                     String origin = message.getBody()
-                    logger.info('Received PING message from and sending response to {}', origin)
+                    logger.info('Received PING message from and sending response back to {}', origin)
                     Message response = new Message()
                     response.setBody(MBOX)
                     response.setCommand('PONG')
                     response.setRoute([origin])
                     po.send(response)
+                    logger.info('Response PONG sent to {}', origin)
                 }
                 else {
+                    logger.info('Received Message {}', id)
                     logger.info("Generating query from received Message {}", id)
                     Query query = Serializer.parse(Serializer.toJson(message.body), Query)
-
                     logger.info("Gathering solr documents")
                     GetSolrDocuments process = new GetSolrDocuments()
-
                     int number_of_documents = command.toInteger()
-
                     Map result = process.answer(query, id, number_of_documents)
-                    //message.setBody(result)
-
                     logger.info("Processed query from Message {}, sending documents back to web",id)
                     Message response = new Message()
                     response.setCommand('solr')
@@ -68,12 +62,9 @@ class Main{
                     response.setBody(result)
                     response.setId(id)
                     response.setParameters(params)
-
                     po.send(response)
-
                     logger.info("Message {} with solr documents sent back to web", id)
                 }
-
             }
         }
         synchronized(lock) { lock.wait() }
@@ -81,13 +72,17 @@ class Main{
         box.close()
         logger.info("Solr service terminated")
     }
-
-    void shutdown(Object lock){
-        logger.info('Received shutdown message, terminating Solr service')
-        synchronized(lock) { lock.notify() }
-
+    static void main(String[] args) {
+        logger.info("Starting Solr service")
+        Object lock = new Object()
+        Thread.start {
+            new Main().run(lock)
+        }
     }
 
+    /**
+     * CODE IS NOT CURRENTLY USED
+     *
     //Checks if:
     // 1) message body is empty
     // 2) command is empty (as of right now, default is 10) THIS IS NOW DONE IN WEB
@@ -111,12 +106,7 @@ class Main{
         }
         return true
     }
-    
-    static void main(String[] args) {
-        logger.info("Starting Solr service, awaiting Message containing query from Web module")
-        Object lock = new Object()
-        Thread.start {
-            new Main().run(lock)
-        }
-    }
+
+
+ **/
 }
